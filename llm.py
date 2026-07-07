2bea4c488eb7a9ce9587881ee301a0a9d8cc1fa8
boedefuelling <lucas@boedefuelling.com>
2026-07-07 16:12:38 +0800

    initial commit

--- llm.py
+++ llm.py
@@ -8,5 +8,9 @@ client = OpenAI(
     base_url=endpoint,
     api_key=api_key
 )
+response = client.responses.create(
+    model=deployment_name,
+    input="What is the capital of France?",
+)
 
-print(client.models.retrieve(deployment_name))
+print(f"answer: {response.output[0]}")
